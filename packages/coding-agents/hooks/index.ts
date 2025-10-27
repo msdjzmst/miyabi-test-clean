786bdd1082b2e886b9d596cdb2ba615385a1b059
Shinya Masuda <msdjzmst@gmail.com>
2025-10-27 13:06:51 +0900

    Add scripts, utils, and packages directories

---
+++ packages/coding-agents/hooks/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Hook System
+ *
+ * Exports all hook-related functionality
+ */
+
+export { HookManager } from './hook-manager';
+export * from './built-in/index';
+export * from '../types/hooks';
